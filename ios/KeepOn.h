12ec80e649d6ecaef97a491676aa6507b669c228
methce <methce@gmail.com>
2020-04-07 13:19:44 -0300

    KeepOn header fix

--- ios/KeepOn.h
+++ ios/KeepOn.h
@@ -1,37 +1,10 @@
-#import "KeepOn.h"
-#import "UIKit/UIKit.h"
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#elif __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import "React/RCTBridgeModule.h"
+#endif
 
-
-@implementation KeepOn
-
-RCT_EXPORT_MODULE();
-
-RCT_EXPORT_METHOD(keepON)
-{
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [[UIApplication sharedApplication] setIdleTimerDisabled:YES];
-    });
-}
-
-RCT_EXPORT_METHOD(keepOFF)
-{
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [[UIApplication sharedApplication] setIdleTimerDisabled:YES];
-    });
-}
-
-RCT_EXPORT_METHOD(unkeepON)
-{
-    dispatch_async(dispatch_get_main_queue(), ^{
-        beginIgnoringInteractionEvents()
-    });
-}
-
-RCT_EXPORT_METHOD(unkeepOFF)
-{
-    dispatch_async(dispatch_get_main_queue(), ^{
-        endIgnoringInteractionEvents()
-    });
-}
-
+@interface KeepOn : NSObject <RCTBridgeModule>
 @end
